1210f8b3e10b96478de764894fd149d28602bc33
Louise Deeth <louisedeeth@gmail.com>
2024-10-04 09:26:29 +0000

    lab2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/navbar';
 import Header from './components/header';
-import Content from './components/Content';
 import Footer from './components/footer';
+import Content from './components/Content';
 
-//Updated the App.js file
+//added links in navbar to routes
 function App() {
   return (
-    <div>
-      <Header />
-      <Content />
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<Header />} />
+        <Route path="/create" element={<Footer />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
