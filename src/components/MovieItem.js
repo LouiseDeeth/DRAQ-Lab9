d133773d764e115d3710f72cf9155b854e0c6113
Louise Deeth <louisedeeth@gmail.com>
2024-10-11 14:52:37 +0000

    Complete Task 3

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
+import { Card } from "react-bootstrap";
+
+
 //Added a movieItem.js file
 const MovieItem = (props) => {
+
+    useEffect(
+        () => {
+            console.log("Movie Item:", props.mymovie);
+        }, [props.mymovie]// Only run this effect when the mymovie prop changes
+    );
     return (
         <div>
-            <h3>{props.mymovie.Title}</h3>
-            <p>{props.mymovie.Year}</p>
-            <img src={props.mymovie.Poster}></img>
-
+            <Card style={{ width: '18rem' }}>
+                <Card.Header>{props.mymovie.Title}</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <Card.Img src={props.mymovie.Poster}></Card.Img>
+                        <p>{props.mymovie.Year}</p>
+                    </blockquote>
+                </Card.Body>
+            </Card>
         </div>
     );
 }
